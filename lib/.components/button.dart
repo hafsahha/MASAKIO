a0cd253fedecbff275df3128bbebe0f2dd88caf2
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-22 01:11:13 +0700

    add textSize and borderColor parameters to Button component

--- lib/.components/button.dart
+++ lib/.components/button.dart
@@ -6,7 +6,19 @@ class Button extends StatelessWidget {
   final double? width;
   final int? backgroundColor;
   final int? textColor;
-  const Button({super.key, required this.onPressed, required this.content, this.width, this.backgroundColor, this.textColor});
+  final double? textSize;
+  final int? borderColor;
+
+  const Button({
+    super.key,
+    required this.onPressed,
+    required this.content,
+    this.width,
+    this.backgroundColor,
+    this.textColor,
+    this.textSize,
+    this.borderColor
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +29,16 @@ class Button extends StatelessWidget {
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           backgroundColor: Color(backgroundColor ?? 0xFF83AEB1),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+          side: borderColor != null
+            ? BorderSide(color: Color(borderColor!), width: 2)
+            : BorderSide.none,
           elevation: 0,
         ),
         child: Text(
           content,
           style: TextStyle(
+            fontSize: textSize ?? 16,
             color: Color(textColor ?? 0xFFFFFFFF),
             fontWeight: FontWeight.bold,
           ),
